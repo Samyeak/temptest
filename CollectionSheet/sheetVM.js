3f715b0d2450a9dd0065c990cb48a588a227dca6
samyeak <samyeakmhj@gmail.com>
2019-05-22 08:09:17 +0545

    Seperated Different Section for Demo Data, Removed Unwanted Comments

--- CollectionSheet/sheetVM.js
+++ CollectionSheet/sheetVM.js
@@ -25,6 +25,7 @@ var CollectionSheet = function (argument) {
 			var accountGroup = this;
 			item = item || {};
 			this.AccountGroupCode = ko.observable(item.AccountGroupCode || "01");
+			this.AccountType = ko.observable(item.AccountType || "04");
 			this.Accounts = ko.observableArray(ko.utils.arrayMap(item.Accounts || [], (x) => new models.Account(x)) || []);
 			this.AccountGroupsTotal = ko.computed(x=> sum(ko.toJS(this.Accounts), 'CreditAmt'));
 		}
@@ -33,10 +34,10 @@ var CollectionSheet = function (argument) {
 			item = item || {};
 			this.Name = ko.observable(item.Name || "John Doe");
 			this.AccountGroups = ko.observableArray(ko.utils.arrayMap(item.AccountGroups || [], (x) => new models.AccountGroup(x)) || []);
-			this.Accounts = ko.observableArray(ko.utils.arrayMap(item.Accounts || [], (x) => new models.Account(x)) || []);
-			this.AccountsTotal = ko.computed(function (argument) {
-				return sum(ko.toJS(member.Accounts), 'InstallmentAmt');
-			});
+			// this.Accounts = ko.observableArray(ko.utils.arrayMap(item.Accounts || [], (x) => new models.Account(x)) || []);
+			// this.AccountsTotal = ko.computed(function (argument) {
+			// 	return sum(ko.toJS(member.Accounts), 'InstallmentAmt');
+			// });
 
 			this.MemberTotal = ko.computed(x=> sum(ko.toJS(this.AccountGroups), 'AccountGroupsTotal'));
 			
@@ -53,38 +54,100 @@ var CollectionSheet = function (argument) {
 
 	var UiElements = {
 		init : function (param) {
-			var account = new models.Account({
-				AccountNo: "001",
-				Balance: 11,
-				AccountGroupCode: "01",
-				AccountType: 1
-			});
-			var account2 = new models.Account({
-				AccountNo: "001",
-				Balance: 11,
-				AccountGroupCode: "02",
-				AccountType: 2
-			});
-			
+			self.Members = ko.observableArray([]);
+			self.AccountGroups = ko.observableArray([]);
+		},
+		demo:function (argument) {
+			var memberZ = {
+				Name : "Laxmi Prasad Devkota",
+				Accounts : [
+					{
+						AccountNo: "001000002",
+						InterestAmt: 500,
+						AccountGroupCode: "01",
+						AccountType: "03"
+					},
+					{
+						AccountNo: "001000023",
+						InterestAmt: 200,
+						AccountGroupCode: "01",
+						AccountType: "03"
+					},
+					{
+						AccountNo: "001000152",
+						InterestAmt: 600,
+						AccountGroupCode: "01",
+						AccountType: "03"
+					}
+				]
+			}; //END of Member Z
 
-			accounts = ko.observableArray([account]);
-			accounts2 = ko.observableArray([account2, account2]);
-
-			// self.AccountsTotal = ko.computed(function (argument) {
-			// 	return sum(ko.toJS(self.Accounts()), 'Balance');
-			// });
-
-
-			/*MEMBER SECTION*/
-			var member = new models.Member({AccountGroups: [{AccountGroupCode: "01", Accounts: ko.toJS(accounts)}, {AccountGroupCode: "02", Accounts: ko.toJS(accounts2)}]});
-			var member2 = new models.Member({AccountGroups: [{AccountGroupCode: "01", Accounts: ko.toJS(accounts)}]});
-
-			self.Members = ko.observableArray([member, member2]);
+			var memberOne = {
+				Name : "Laxmi Prasad Devkota",
+				AccountGroups : [
+					{
+						AccountGroupCode : "01",
+						AccountType: "03",
+						Accounts : [
+							{
+								AccountNo: "001000002",
+								InterestAmt: 500,
+								AccountGroupCode: "01",
+								AccountType: "03"
+							},
+							{
+								AccountNo: "001000023",
+								InterestAmt: 200,
+								AccountGroupCode: "01",
+								AccountType: "03"
+							}
+						]
+					},
+					{
+						AccountGroupCode : "02",
+						AccountType: "04",
+						Accounts : [
+							{
+								AccountNo: "002001254",
+								CreditAmt: 1200,
+								AccountGroupCode: "02",
+								AccountType: "04"
+							},
+							{
+								AccountNo: "002001258",
+								CreditAmt: 1800,
+								AccountGroupCode: "02",
+								AccountType: "04"
+							}
+						]
+					},
+					{
+						AccountGroupCode : "03",
+						AccountType: "04",
+						Accounts : [
+							{
+								AccountNo: "003006545",
+								CreditAmt: 2200,
+								AccountGroupCode: "03",
+								AccountType: "04"
+							},
+							{
+								AccountNo: "003006589",
+								CreditAmt: 2800,
+								AccountGroupCode: "03",
+								AccountType: "04"
+							}
+						]
+					}
+				]
+			}; //END of Member One
+			self.Members([new models.Member(memberOne)/*, new models.Member(memberTwo)*/]);
 		}
 	};
 
 	var init = function (param) {
 		UiElements.init();
+		UiElements.demo()
 	};
 
 	init();
